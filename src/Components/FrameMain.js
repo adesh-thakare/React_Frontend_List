8498613f156456e8261341aa0c348d38c23eb763
Adesh Thakare <adeshthakare@Adeshs-MacBook-Air.local>
2024-04-14 15:45:43 -0700

    railway url

--- src/Components/FrameMain.js
+++ src/Components/FrameMain.js
@@ -14,7 +14,7 @@ const FrameMain = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('https://localhost:8000/top-authors');
+                const response = await axios.get('https://nodebackend-production-c21f.up.railway.app/top-authors');
                 setData(response.data);
                 setCountMembers(response.data.length);
                 //console.log(response.data);
